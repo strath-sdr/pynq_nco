5818f3aa53bd3cc61362c3c64882a2da474ecf4d
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2023-01-23 15:38:01 +0000

    Update doc string and fix initialisation bug

--- pynq_nco/data_inspector.py
+++ pynq_nco/data_inspector.py
@@ -5,20 +5,30 @@ import numpy as np
 _MAX_TRANSFER = 4096
 
 class DataInspector(DefaultHierarchy):
-    
+    """A class to control the Data Inspector hierarchy."""
+
     @staticmethod
     def checkhierarchy(description):
+        """The DataInspector class must contain an AXI DMA and Packet Generator."""
         if 'axi_dma' in description['ip'] \
            and 'packet_generator' in description['ip']:
             return True
         return False 
     
     def __init__(self, description):
+        """Class constructor for the DataInspector hierarchy."""
         super().__init__(description=description)
         self._recv_buffer = allocate(shape=(_MAX_TRANSFER*2), dtype=np.int16)
         temp = self.transfer(_MAX_TRANSFER)
         
     def transfer(self, packetsize):
+        """Transfer samples from the FPGA logic fabric to the Processing System.
+        
+        Parameters
+        ----------
+            packetsize : int
+                number of samples to transfer
+        """
         self.packet_generator.packetsize = int(packetsize)
         self.packet_generator.reset = 0
         self.axi_dma.recvchannel.transfer(self._recv_buffer)
--- pynq_nco/nco.py
+++ pynq_nco/nco.py
@@ -4,9 +4,12 @@ _SAMPLE_FREQUENCY = 100e6
 _PHASE_WIDTH = 16
 
 class NumericalOscillator(DefaultIP):
+    """A class to control an NCO IP Core."""
     def __init__(self, description):
+        """Create a Numerical Oscillator object that controls the NCO IP Core in the PL."""
         super().__init__(description=description)
         self.frequency = 1e6
+        self.real_enable()
         
     bindto = ["strathsdr.org:pynq:nco:1.0"]
         
@@ -36,11 +39,13 @@ class NumericalOscillator(DefaultIP):
         
     @property
     def frequency(self):
+        """The output frequency of the NCO."""
         reg = self._phase*(2**-16)
         return (_SAMPLE_FREQUENCY*reg)/(2**_PHASE_WIDTH)
         
     @frequency.setter
     def frequency(self, value):
+        """Sets the desired frequency of the NCO."""
         if (value > _SAMPLE_FREQUENCY/2) and (value <= 0):
             raise ValueError(''.join(['Select a frequency between 1 and ', str(_SAMPLE_FREQUENCY)]))
         reg = ((value*(2**_PHASE_WIDTH))/_SAMPLE_FREQUENCY)
@@ -48,26 +53,31 @@ class NumericalOscillator(DefaultIP):
         
     @property
     def gain(self):
+        """The output gain of the NCO."""
         return self._gain*(2**-30)
     
     @gain.setter
     def gain(self, value):
+        """Sets the NCO gain."""
         if (value > 1) or (value < -1):
             raise ValueError('Select a gain between -1 and 1')
         self._gain = int(value*(2**30))
         
     def complex_enable(self):
+        """Enables the Cosine and Sine wave output of the NCO."""
         reg = self._control
         reg |= 0x00000003
         self._control = reg
         
     def real_enable(self):
+        """Enables the Cosine wave output only of the NCO."""
         reg = self._control
         reg &= 0xFFFFFFFC
         reg |= 0x00000001
         self._control = reg
         
     def disable(self):
+        """Disables the Cosine and Sine wave output of the NCO."""
         reg = self._control
         reg &= 0xFFFFFFFC
         self._control = reg
