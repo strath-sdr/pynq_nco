cb14c346cc02114488e0347e52bc8ff875f8572e
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2023-01-23 10:06:48 +0000

    Add ZCU111 Design

---
+++ boards/ZCU111/pynq_nco/make_block_design.tcl
@@ -0,0 +1,18 @@
+set overlay_name "block_design"
+set design_name "pynq_nco"
+set iprepo_dir ./../../ip/iprepo
+
+# Create project
+create_project ${overlay_name} ./${overlay_name} -part xczu28dr-ffvg1517-2-e
+set_property board_part xilinx.com:rfsoc2x2:part0:1.1 [current_project]
+set_property target_language VHDL [current_project]
+
+# Set IP repository paths
+set_property ip_repo_paths $iprepo_dir [current_project]
+update_ip_catalog
+
+# Add constraints
+add_files -fileset constrs_1 -norecurse ./constraints.xdc
+
+# Make block design
+source ./${design_name}.tcl
